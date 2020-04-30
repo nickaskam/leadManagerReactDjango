fa43e98feeeb0d5abb30b03e9c77d5d415fa8cd3
Nick Askam <asknickfilms@gmail.com>
2020-04-30 13:28:26 -0500

    adding redux and abilities to add and delete leads

--- leadmanager/frontend/src/components/layout/Header.js
+++ leadmanager/frontend/src/components/layout/Header.js
@@ -1,21 +1,29 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export class Header extends Component {
-    render() {
-        return (
-            <nav className="navbar navbar-expand-sm navbar-light bg-light">
-                <a className="navbar-brand" href="#">Lead Manager</a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav mr-auto">
-                    
-                    </ul>
-                </div>
-            </nav>
-        )
-    }
+  render() {
+    return (
+      <nav className="navbar navbar-expand-sm navbar-light bg-light">
+        <a className="navbar-brand" href="#">
+          Lead Manager
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav mr-auto"></ul>
+        </div>
+      </nav>
+    );
+  }
 }
 
-export default Header
+export default Header;
