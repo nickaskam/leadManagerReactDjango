a4b058c1356201d80d17759ae6af0a3a8a2a78af
Nick Askam <asknickfilms@gmail.com>
2020-05-01 21:14:22 -0500

    updating heroku file

--- leadmanager/leadmanager/settings.py
+++ leadmanager/leadmanager/settings.py
@@ -22,12 +22,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('LEADMANAGER_KEY')
+SECRET_KEY = os.environ.get('LM_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost']
 
 
 # Application definition
