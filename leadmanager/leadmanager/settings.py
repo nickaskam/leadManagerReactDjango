18f5cdc8bf77f3dc0f60cb99160a76b46d529981
Nick Askam <asknickfilms@gmail.com>
2020-05-01 20:23:36 -0500

    adding in dependencies and procfiles

--- leadmanager/leadmanager/settings.py
+++ leadmanager/leadmanager/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
+import django_heroku
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -20,10 +22,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'si!stb+21-l8(&6s-_qjhgl+f@!$xt0k02!)2ob5p5@7_su6-s'
+SECRET_KEY = os.environ.get('LEADMANAGER_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -127,3 +129,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+django_heroku.settings(locals())
